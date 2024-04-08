1d3ae8b7707cd60add757d2306c0af84a39e1f14
Yunkai Zheng <yunkai.zheng@unity.cn>
2024-04-08 18:02:28 +0800

    init, cocos build new webgl2 application. copy it to wechatWebGL2host, ready to workaround in this folder. keep wechatWegGL2wechat clean for comparison

--- build/wechatgame/test.js
+++
@@ -1,7 +0,0 @@
-// function showTest(num){
-//   console.log(`showTest: ${num}`);
-// }
-function showOverride(num){
-  console.log(`showOverride: ${num}`);
-}
-module.exports = {/*showTest,*/showOverride};
